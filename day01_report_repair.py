5924d02229baf06974b61b04f33ccf1a64c9daf7
IronhandedLayman <nathaniel.dean@alumni.purdue.edu>
2020-12-01 00:42:49 -0500

    day1-star2

--- day01_report_repair.py
+++ day01_report_repair.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+from functools import reduce
+
 DAY=1
 
 def dataLocation(day):
@@ -13,5 +15,11 @@ def loadData(day):
 
 def star1_sumto2020():
     procData=[int(x) for x in loadData(DAY).split("\n") if len(x)>0]
-    fatedPair=[(x,y) for x in procData for y in procData if x+y==2020][0]
+    fatedPair=[[x,y] for x in procData for y in procData if x+y==2020][0]
     return fatedPair[0]*fatedPair[1]
+
+def star2_triplesumto2020():
+    procData=[int(x) for x in loadData(DAY).split("\n") if len(x)>0]
+    fatedPair=[[x,y,z] for x in procData for y in procData for z in procData if x+y+z==2020][0]
+    return reduce(lambda x,y: x*y, fatedPair) 
+
